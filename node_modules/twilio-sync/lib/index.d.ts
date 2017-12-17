6999aa62c5c39c2f2fea6e32be360793c17e57f2
Ameer Badri <ameerbadri@gmail.com>
2017-12-17 17:42:52 +0000

    streamline code

--- node_modules/twilio-sync/lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { SyncClient } from './client';
-import { SyncDocument } from './syncdocument';
-import { SyncList } from './synclist';
-import { ListItem as SyncListItem } from './listitem';
-import { SyncMap } from './syncmap';
-import { MapItem as SyncMapItem } from './mapitem';
-export { SyncClient, SyncDocument, SyncList, SyncListItem, SyncMap, SyncMapItem };
-export default SyncClient;
--- node_modules/twilio-sync/lib/syncNetworkError.d.ts
+++
@@ -1,7 +0,0 @@
-import { SyncError } from './syncerror';
-declare class SyncNetworkError extends SyncError {
-    body: any;
-    constructor(message: string, status: number, code: number, body: any);
-}
-export { SyncNetworkError };
-export default SyncNetworkError;
